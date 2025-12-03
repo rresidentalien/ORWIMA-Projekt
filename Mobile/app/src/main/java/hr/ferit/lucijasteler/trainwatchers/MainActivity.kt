b82040de426acf6def4ee3f21311110fedaca342
rresidentalien <heyitslu.s@gmail.com>
2025-12-03 21:59:54 +0100

    Add home screen and title

--- Mobile/app/src/main/java/hr/ferit/lucijasteler/trainwatchers/MainActivity.kt
+++ Mobile/app/src/main/java/hr/ferit/lucijasteler/trainwatchers/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import hr.ferit.lucijasteler.trainwatchers.ui.theme.HomeScreen
 import hr.ferit.lucijasteler.trainwatchers.ui.theme.TrainwatchersTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +20,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TrainwatchersTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                HomeScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TrainwatchersTheme {
-        Greeting("Android")
     }
 }
---
+++ Mobile/app/src/main/java/hr/ferit/lucijasteler/trainwatchers/ui/theme/HomeScreen.kt
@@ -0,0 +1,30 @@
+package hr.ferit.lucijasteler.trainwatchers.ui.theme
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Preview
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier) {
+    Column() {
+        Title("Trainwatchers")
+    }
+}
+
+@Composable
+fun Title(title : String) {
+    Text(text = title,
+        color = Color.Black,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 20.sp,
+        modifier = Modifier.fillMaxWidth().padding(50.dp))
+}
