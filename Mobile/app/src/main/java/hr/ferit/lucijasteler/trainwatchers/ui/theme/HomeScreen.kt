3a86d0cf1942842ffccbfc265311cf27a7a41cc9
rresidentalien <heyitslu.s@gmail.com>
2025-12-04 16:59:33 +0100

    Add subtitle and counter to home screen

--- Mobile/app/src/main/java/hr/ferit/lucijasteler/trainwatchers/ui/theme/HomeScreen.kt
+++ Mobile/app/src/main/java/hr/ferit/lucijasteler/trainwatchers/ui/theme/HomeScreen.kt
@@ -1,13 +1,17 @@
 package hr.ferit.lucijasteler.trainwatchers.ui.theme
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -15,8 +19,15 @@ import androidx.compose.ui.unit.sp
 @Preview
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
-    Column() {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(color = Color(0xFFFAEBD7))
+    ) {
         Title("Trainwatchers")
+        Subtitle("Lucija")
+        Counter(3)
+        LazyColumn() { }
     }
 }
 
@@ -25,6 +36,27 @@ fun Title(title : String) {
     Text(text = title,
         color = Color.Black,
         fontWeight = FontWeight.SemiBold,
-        fontSize = 20.sp,
-        modifier = Modifier.fillMaxWidth().padding(50.dp))
+        fontSize = 30.sp,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 50.dp, start = 20.dp, bottom = 10.dp))
+}
+
+@Composable
+fun Subtitle(name : String) {
+    Text(text = "Welcome, $name!",
+        color = Color.DarkGray,
+        fontSize = 16.sp,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 20.dp))
+}
+
+@Composable
+fun Counter(count : Int) {
+    Text(text = "You have entered $count trains so far",
+        textAlign = TextAlign.Center,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 30.dp))
 }
