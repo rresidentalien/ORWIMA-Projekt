d07d16ca33a8fd6e83505c431558fb28541b5188
rresidentalien <heyitslu.s@gmail.com>
2025-12-17 22:53:56 +0100

    Add column where trains will be displayed

--- Mobile/app/src/main/java/hr/ferit/lucijasteler/trainwatchers/ui/theme/MyTrains.kt
+++ Mobile/app/src/main/java/hr/ferit/lucijasteler/trainwatchers/ui/theme/MyTrains.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -25,6 +26,7 @@ fun MyTrains(modifier: Modifier = Modifier) {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         MyTrainsHeader()
+        LazyColumn() { }
     }
 }
 
