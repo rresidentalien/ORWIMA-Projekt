195af99e95d6c5fb6603380b170a1a6fe6b1fe0f
rresidentalien <heyitslu.s@gmail.com>
2025-12-22 15:43:09 +0100

    Finish train list and card

--- Mobile/app/src/main/java/hr/ferit/lucijasteler/trainwatchers/ui/theme/HomeScreen.kt
+++ Mobile/app/src/main/java/hr/ferit/lucijasteler/trainwatchers/ui/theme/HomeScreen.kt
@@ -1,26 +1,34 @@
 package hr.ferit.lucijasteler.trainwatchers.ui.theme
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import coil.compose.rememberAsyncImagePainter
+import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.AsyncImage
 import hr.ferit.lucijasteler.trainwatchers.data.TrainViewModel
+import java.text.SimpleDateFormat
 import java.util.Date
+import java.util.Locale
 
 @Composable
-fun HomeScreen(modifier: Modifier = Modifier, viewModel : TrainViewModel = TrainViewModel()) {
+fun HomeScreen(modifier: Modifier = Modifier, viewModel : TrainViewModel = viewModel()) {
     Column(
         modifier = modifier
             .fillMaxSize()
@@ -29,8 +37,8 @@ fun HomeScreen(modifier: Modifier = Modifier, viewModel : TrainViewModel = Train
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Title("Trainwatchers")
-        Subtitle("Lucija")
         Counter(viewModel)
+        TrainsList(viewModel)
     }
 }
 
@@ -45,23 +53,19 @@ fun Title(title : String) {
 }
 
 @Composable
-fun Subtitle(name : String) {
-    Text(text = "Welcome, $name!",
-        color = Color.DarkGray,
-        fontSize = 16.sp
-    )
-}
-
-@Composable
 fun Counter(viewModel : TrainViewModel) {
-    if (viewModel.trains.isEmpty())
+    if (viewModel.trains.isEmpty()) {
         Text(text = "No trains found. Enter some!",
             modifier = Modifier.padding(top = 30.dp),
             color = Brown)
-    else
-        Text(text = "You have entered ${viewModel.trains.size} trains so far",
+    }
+    else {
+        Text(text = "You have entered ${viewModel.trains.size} trains so far.",
             modifier = Modifier.padding(top = 30.dp),
             color = Brown)
+        Text(text = "Recently entered trains:",
+            color = Brown)
+    }
 }
 
 @Composable
@@ -69,15 +73,24 @@ fun TrainCard(imageResource : String, model : String, city : String, date : Date
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .background(color = Brown)
+            .background(color = Brown, shape = RoundedCornerShape(8.dp))
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
     ){
-        Text(text = model)
-        Text(text = "$city, $date")
-        Image(
-            painter = rememberAsyncImagePainter(model = imageResource),
-            contentDescription = model,
-            modifier = Modifier.fillMaxWidth()
-        )
+        val formattedDate = SimpleDateFormat("dd.MM.yyyy.", Locale.getDefault()).format(date)
+        Text(text = model, color = AntiqueWhite, fontWeight = FontWeight.Bold, fontSize = 20.sp)
+        Text(text = "$city, $formattedDate", color = AntiqueWhite)
+        if (imageResource.isNotEmpty()) {
+            AsyncImage(
+                model = imageResource,
+                contentDescription = model,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp)
+                    .clip(RoundedCornerShape(8.dp)),
+                contentScale = ContentScale.Crop,
+            )
+        }
     }
 }
 
@@ -86,13 +99,18 @@ fun TrainsList(viewModel : TrainViewModel) {
     LazyColumn(
         modifier = Modifier
             .fillMaxSize()
-            .padding(vertical = 20.dp)
+            .padding(20.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
-        items(viewModel.trains.size) {
-            TrainCard(imageResource = viewModel.trains[it].images[0],
-                model = viewModel.trains[it].model,
-                city = viewModel.trains[it].city,
-                date = viewModel.trains[it].date)
+        items(viewModel.trains) {train ->
+            if (train.images.isNotEmpty()) {
+                TrainCard(
+                    imageResource = train.images[0],
+                    model = train.model,
+                    city = train.city,
+                    date = train.date
+                )
+            }
         }
     }
 }
