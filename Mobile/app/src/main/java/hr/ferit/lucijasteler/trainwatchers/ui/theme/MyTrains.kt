89b8a9e613453ef71ba0e64fd90742fed3702953
Lucija <heyitslu.s@gmail.com>
2026-01-13 16:24:02 +0100

    Add favourites button (unfinished)

--- Mobile/app/src/main/java/hr/ferit/lucijasteler/trainwatchers/ui/theme/MyTrains.kt
+++ Mobile/app/src/main/java/hr/ferit/lucijasteler/trainwatchers/ui/theme/MyTrains.kt
@@ -3,8 +3,13 @@ package hr.ferit.lucijasteler.trainwatchers.ui.theme
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -28,11 +33,23 @@ fun MyTrains(modifier: Modifier = Modifier, viewModel : TrainViewModel = viewMod
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Title("My trains")
+        FavouriteButton()
         TrainsList(viewModel)
     }
 }
 
 @Composable
-fun FavouriteButton(modifier: Modifier = Modifier) {
+fun FavouriteButton() {
+    OutlinedButton(
+        onClick = { /*TODO*/ },
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(40.dp)
+            .padding(start = 16.dp, end = 16.dp),
+        shape = RoundedCornerShape(8.dp),
+        colors = ButtonDefaults.outlinedButtonColors(contentColor = Brown),
 
+    ) {
+        Text("View favourite trains")
+    }
 }
